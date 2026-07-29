120dfc6e54ddc00acb8691e82d6edbe47e00419a
faridfathurahman <faridfathurahman94@gmail.com>
2026-07-30 00:08:37 +0900

    Update send-line.js

--- netlify/functions/send-line.js
+++ netlify/functions/send-line.js
@@ -8,29 +8,35 @@ exports.handler = async (event) => {
 
   const CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 
-  const { userId, message } = JSON.parse(event.body);
+  const body = JSON.parse(event.body);
+  const message = body.message;
 
-  const response = await fetch("https://api.line.me/v2/bot/message/push", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`
-    },
-    body: JSON.stringify({
-      to: userId,
-      messages: [
-        {
-          type: "text",
-          text: message
-        }
-      ]
-    })
-  });
+  // Ganti nanti dengan Group ID setelah kita mendapatkannya
+  const targets = [
+    "Ud7e29225e426ea4d509d2edac5384028"
+  ];
 
-  const result = await response.text();
+  for (const target of targets) {
+    await fetch("https://api.line.me/v2/bot/message/push", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`
+      },
+      body: JSON.stringify({
+        to: target,
+        messages: [
+          {
+            type: "text",
+            text: message
+          }
+        ]
+      })
+    });
+  }
 
   return {
-    statusCode: response.status,
-    body: result
+    statusCode: 200,
+    body: "OK"
   };
 };
